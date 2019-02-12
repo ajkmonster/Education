9b4eae65f6bad27a62b041de5df1f6d6b2672841
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-02-12 16:59:14 -0500

    better use of printing education by join method

--- src/Person.java
+++ src/Person.java
@@ -4,13 +4,16 @@ public class Person {
     //composition has-a relationship
     private Job job;
     private Education education;
+    private ArrayList<String> education1;
+    private String educationString;
     public Person(){
+        education1 = new ArrayList<>();
         this.job=new Job();
         job.setSalary(60000L);
         job.setRole("Accountant");
         job.setId(3456);
         this.education=new Education();
-        education.setSchools(new ArrayList<>());
+        education.setSchools(education1);
     }
     public long getSalary() {
         return job.getSalary();
@@ -18,6 +21,10 @@ public class Person {
 
     @Override
     public String toString(){
-        return  job.toString()+education.toString();
+        return  job.toString()+"Education:   " +makeString()+ "\n";
+    }
+    public String makeString(){
+        educationString = educationString.join(",",education1);
+        return educationString;
     }
 }
